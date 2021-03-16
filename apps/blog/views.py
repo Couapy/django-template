1c48d14113f2c9cf948259802439e63a7a0e3245
Couapy <mael@marchand.cloud>
2021-03-16 11:09:30 +0100

    feat(apps): add blog application

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def list(request):
+    posts = Post.objects.filter(available=True).order_by("-publication_date")
+    return render(request, 'blog/list.html', context={'posts': posts})
+
+
+def post(request, slug):
+    post = get_object_or_404(Post, slug=slug, available=True)
+    return render(request, 'blog/post.html', context={'post': post})
