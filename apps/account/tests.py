8cba6ac24d6e307493af28544fb4ece7f8f367db
Maël <39948565+Couapy@users.noreply.github.com>
2021-03-19 12:51:01 +0000

    test(account): add test for account application

--- apps/account/tests.py
+++ apps/account/tests.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile
+
+
+class ProfileTestCase(TestCase):
+    """Tests for profile model."""
+
+    def test_auto_create_profile(self):
+        """Automatically create profile for new users."""
+        user = User.objects.create(username="user_new_profile")
+        profile = Profile.objects.last()
+        self.assertNotEqual(user.profile, None)
+        self.assertEqual(user.profile, profile)
